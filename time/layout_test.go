b7f4c5f1afc0649dd941e4f0877cd2b11fe671ae
itang <live.tang@gmail.com>
2013-10-18 09:21:17 +0800

    use data-driver-test

--- time/layout_test.go
+++ time/layout_test.go
@@ -10,8 +10,18 @@ import (
 func TestChinaDateLayout(t *testing.T) {
 	testTime := time.Date(2009, time.November, 10, 15, 12, 10, int(time.Millisecond*time.Duration(300)), time.Local)
 
-	assert.Equal(t, "2009-11-10 15:12:10.3", testTime.Format(ChinaDefaultDateTimeFull))
-	assert.Equal(t, "2009-11-10 15:12:10", testTime.Format(ChinaDefaultDateTime))
-	assert.Equal(t, "2009-11-10", testTime.Format(ChinaDefaultDate))
-	assert.Equal(t, "15:12:10", testTime.Format(ChinaDefaultTime))
+	var tests = []struct {
+		time     time.Time
+		layout   string
+		formated string
+	}{
+		{testTime, ChinaDefaultDateTimeFull, "2009-11-10 15:12:10.3"},
+		{testTime, ChinaDefaultDateTime, "2009-11-10 15:12:10"},
+		{testTime, ChinaDefaultDate, "2009-11-10"},
+		{testTime, ChinaDefaultTime, "15:12:10"},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.formated, test.time.Format(test.layout))
+	}
 }
